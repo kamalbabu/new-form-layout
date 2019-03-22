c43be58d25b25a5f4de6f682c56da3916802309c
unknown <kamalbabu57@gmail.com>
2019-03-22 17:07:13 +0000

    First Commit

---
+++ src/components/side-nav/SideNav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import logo from "../assets/mdb-react.png";
+
+const TopNavigation = () => {
+    return (
+        <div className="sidebar-fixed position-fixed">
+           hello
+        </div>
+    );
+}
+
+export default TopNavigation;
---
+++ src/pages/dashboard/Dashboard.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import './Dashboard.css';
+
+import { MDBBtn, MDBIcon } from "mdbreact";
+
+import { MDBInput } from "mdbreact";
+// import { MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavbarToggler, MDBCollapse, MDBNavItem, MDBNavLink, MDBIcon } from 'mdbreact';
+import { MDBContainer, MDBRow, MDBCol } from "mdbreact";
+class Dashboard extends Component {
+    render() {
+        return (
+            <MDBContainer>
+                <MDBRow className="vertical-align">
+                    <MDBCol md="4" sm="12" className="offset-md-4 flex">
+                        <MDBInput label="Please provide url to file" />
+                        <MDBBtn color="primary" >
+                            <MDBIcon icon="magic" className="mr-2" /> Start
+                        </MDBBtn>
+                    </MDBCol>
+                </MDBRow>
+            </MDBContainer>
+        )
+    }
+}
+
+export default Dashboard
